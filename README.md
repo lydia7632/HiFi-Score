b4107c93c79091a6885a59cac429a94d5c8bdcf2
lydia7632 <yzw1017@icloud.com>
2024-09-27 01:49:38 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # HiFi-Score
-This is the official implementation for HiFi-Score: Fine-grained Image Description Evaluation with Hierarchical Parsing Graphs (ECCV2024).
+This is the official implementation for HiFi-Score: Fine-grained Image Description Evaluation with Hierarchical Parsing Graphs \[ECCV2024\].
 
 ## Test HiFi-Score on Flickr8K-Expert
 ```
--- test_flickr8k_exp.py
+++ test_flickr8k_exp.py
@@ -2,7 +2,7 @@ import json
 from score import print_score
 
 dataset = 'flickr8k_exp'
-glip_res_path = 'blip_matching_results/flickr8k_exp_modeltype-vg_dense_embsize-256_globimgfeat-True_splitcand-False_part0.json'
+glip_res_path = 'matching_results/flickr8k_exp_modeltype-vg_dense_embsize-256_globimgfeat-True_splitcand-False_part0.json'
 
 with open(glip_res_path, 'r') as f:
     res_data = json.load(f)
